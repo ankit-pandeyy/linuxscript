92b0dbcc8cf28b1a2ea41444635eb3322eb8870d
Himanshu Pandey <60029929+ankit-pandeyy@users.noreply.github.com>
2020-02-02 14:15:04 +0530

    Update diskutilisation.sh

--- diskutilisation.sh
+++ diskutilisation.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 CURRENT=$(df -h | grep /dev/mapper/myvol-usr | awk '{ print $5}' | sed 's/%//g')
-d=$(df -h | grep /dev/mapper/myvol-usr | awk '{ print $2,$3,$4,$5,$6 }')
+df -h /usr /var | awk '{ print $2,$3,$4,$5,$6 }' > /home/himanshu/disk.txt
+file= cat /home/himanshu/disk.txt
 THRESHOLD=80
-
+d=$(date)
+h=$(hostname)
 if [ "$CURRENT" -gt "$THRESHOLD" ] ; then
-    nail -s 'Disk Space Alert' himanshu.pandey@payu.in << EOF
-Your Disk utilisation is critical . Current status is: $d on the server 10.50.12.165 $(hostname) at $(date)
-EOF
+nail -s "Disk Utilisation is high at $d on Server $h(Server IP)"  mail@gmail.com < /home/himanshu/disk.txt
 fi
